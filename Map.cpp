460579a6006805e55e1743649865d54f18935fea
bangseungho <suengho2257@naver.com>
2022-12-10 21:39:15 +0900

    Revert "Revert "Merge branch 'BangSeungHo'""
    This reverts commit 693f57d1a66dcaaafbdbaae02584e7f884e13d74.

--- Map.cpp
+++ Map.cpp
@@ -5,9 +5,8 @@ Map::Map() : UIMesh(this)
 	name = "Map";
 	image_file = "map.png";
 
-	// 초기 위치, 크기 설정
-	transform.local->position.x -= 0.5;
-	transform.local->position.y -= 0.7;
+	transform.local->position.x -= 0.67;
+	transform.local->position.y -= 0.68;
 
 	Render::uiRender->AddObject(this);
 }
